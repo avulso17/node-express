233c3de998f9d10772308adfb2f49b6616fa127e
Felipe Mateus <felipe_mateus08@hotmail.com>
2023-01-12 12:39:37 -0300

    fix: vercel.json prod 5

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import app from './src/app.js'
 
 const port = process.env.PORT || 4004
 
+app.get('/', (_req, res) => {
+  res.json({ message: 'Hello World' })
+})
+
 app.listen(port, () => {
   console.log(`Server running at port http://localhost:${port}`)
 })
