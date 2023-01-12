99014a57025b9023e324bd4da2630cab350ac3fa
Felipe Mateus <felipe_mateus08@hotmail.com>
2023-01-12 15:27:35 -0300

    fix: routes error 500

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-import express from 'express'
 import app from './src/app.js'
-import routes from './src/routes/index.js'
 
 const port = process.env.PORT || 4004
-
-app.use(express.json({ extended: false }))
-
-app.use('/', routes)
 
 app.listen(port, () => {
   console.log(`Server running at port http://localhost:${port}`)
